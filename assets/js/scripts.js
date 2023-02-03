cf18ac2fd78c1b57727074e453a4e4047afed5e5
rowenadinata <rowena.dinata@gmail.com>
2023-02-03 07:55:38 +0700

    added hover color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,3 +14,16 @@ btn.on('click', e => {
 
 });
 
+const colors = [ "ColorBlue", "ColorPink", "ColorGreen", "ColorYellow" ];
+const cards = document.querySelectorAll('.list-Card');
+const items = [];
+for (let i = 0; i < cards.length; i++) {
+    items.push(cards[i]);
+}
+items.map(x => {
+    const s = Math.floor(Math.random() * colors.length);
+    const el = x.querySelectorAll('a');
+    for (let obj of el) {
+        obj.classList.add(colors[s])
+    }
+});
